2ac4f579b81f7bb18425864678bc6e7a62c7ec5b
Shadow-Vanguard <roshanv334@gmail.com>
2025-03-18 00:21:20 +0530

    GoogleZzz

--- roadside_assistance/roadside_assistance/settings.py
+++ roadside_assistance/roadside_assistance/settings.py
@@ -55,11 +55,11 @@ INSTALLED_APPS = [
     'django_extensions',
     'roadside_app',   
     'social_django',  
-    'corsheaders',
+    # 'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
