3ba7987bbd2f2ab231935b185be353006c2c6f2b
Shadow-Vanguard <roshanv334@gmail.com>
2025-03-18 07:16:07 +0530

    DB migrate

--- roadside_assistance/roadside_assistance/settings.py
+++ roadside_assistance/roadside_assistance/settings.py
@@ -38,6 +38,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "https://a6a9-136-232-57-110.ngrok-free.app"
+    "https://road-guardian-1.onrender.com/"
 ]
 
 # Get additional trusted origins from environment variable
@@ -102,34 +103,34 @@ WSGI_APPLICATION = 'roadside_assistance.wsgi.application'
 
 #databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'new_roadside_assistance',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1, character_set_server=utf8mb4, collation_server=utf8mb4_unicode_ci",
-        }
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'roadside_supplymany',
-#         'USER': 'roadside_supplymany',
-#         'PASSWORD': 'f719dde48b162c42612de645efacf3572aabbfdf',
-#         'HOST': 'h2ch9.h.filess.io',
-#         'PORT': '3307',
+#         'NAME': 'new_roadside_assistance',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
 #         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#             'charset': 'utf8mb4',
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1, character_set_server=utf8mb4, collation_server=utf8mb4_unicode_ci",
 #         }
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'roadside_supplymany',
+        'USER': 'roadside_supplymany',
+        'PASSWORD': 'f719dde48b162c42612de645efacf3572aabbfdf',
+        'HOST': 'h2ch9.h.filess.io',
+        'PORT': '3307',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
